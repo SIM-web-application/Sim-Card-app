29a58da24ed289496ed06e8d508e38434a726b06
vanthao98 <levanthaolvt228@gmail.com>
2024-08-09 08:48:41 +0700

    create front by nuxt3.12 and responsive QuangCao components

---
+++ frontend/app.vue
@@ -0,0 +1,22 @@
+import { NuxtLayout } from './.nuxt/components';
+<template>
+  <div>
+    <NuxtLayout >
+      <header class="my-3">        
+        <nav class="flex justify-between p-2">
+          <div>
+            <p><span>icon</span>Viettel</p>
+          </div>        
+          <ul>
+            <li class="flex gap-3">
+              <NuxtLink class="btn bg-slate-200" to="/">Home</NuxtLink>
+              <NuxtLink class="btn bg-slate-200" to="/event">Event</NuxtLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <NuxtPage />
+      <footer class="bg-slate-500 h-20 text-black">FOOTER</footer>
+    </NuxtLayout>
+  </div>
+</template>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div class="text-5xl text-rose-500 font-bold border-t-2">
+            <img class="w-full" src="/vietel.png" alt="">
+        </div> 
+        <QuangCao /> 
+    </div>
+</template>
+
+<script setup>
+    // import QuangCao from '~/components/QuangCao.vue'
+</script>
+
+<style scoped>
+
+</style>
